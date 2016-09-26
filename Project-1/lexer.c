cb817696807c3590222b462561e85cbaf6339b2c
cwboyer95 <cwboyer95@gmail.com>
2016-09-26 18:49:42 -0400

    Starter

--- Project-1/lexer.c
+++ Project-1/lexer.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
 
+int main(int argc,char* argv[]){
+    FILE *ifp = fopen(argv[1],"r");
+    char temp[13];
+  
+  while(!feof(ifp)){
+    temp = fscanf(ifp);
+    
+    StringReader(temp);
+  }
+  
+    return 0;
+}
