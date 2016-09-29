946e51352792a54e7ce6f3c96bc8cfe30f2fa614
barrond <barrond@users.noreply.github.com>
2016-09-29 19:09:46 -0400

    Update lexer.c
    Created PrintLines function

--- Project-1/lexer.c
+++ Project-1/lexer.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <ctype.h>
 
-void PrintLines(int input);
+void PrintLines(int commentsIncluded, FILE *ifp);
 void PrintTokens(FILE *ifp);
 
 typedef enum token {nulsym = 1, identsym = 2, numbersym = 3, plussym = 4, minussym = 5, multsym = 6, slashsym = 7, oddsym = 8,
@@ -19,11 +19,18 @@ int main(int argc, char* argv[]){
 	//Checks for --source or --clean command arguments for printing
 	if(argv[2] != NULL){
 		for(i = 2; i < argc; i++){
-
-			if(strcmp(argv[i],"--source"))
-				PrintLines(0);
-			if(strcmp(argv[i],"--clean"))
-				PrintLines(1);
+			if(strcmp(argv[i],"--source")==0)
+            {
+                // Print with comments
+                PrintLines(1, ifp);
+                rewind(ifp);
+            }
+			if(strcmp(argv[i],"--clean")==0)
+            {
+                // Print without comments
+                PrintLines(0, ifp);
+                rewind(ifp);
+            }
 		}
 	}
 
@@ -37,8 +44,75 @@ int main(int argc, char* argv[]){
 
 // Prints output if --source or --clean are given as command arguments
 //if commentsIncluded = 1, print comments. If 0, don't.
-void PrintLines(int commentsIncluded){
+void PrintLines(int commentsIncluded, FILE *ifp){
+    // Declare variables
+    char current;
+    int halt;
 
+    // Header for printing without comments
+    if(commentsIncluded==0)
+    {
+        printf("\nsource code without comments:\n");
+        printf("-----------------------------\n");
+    }
+    // Header for printing with comments
+    else
+    {
+        printf("\nsource code:\n");
+        printf("------------\n");
+    }
+
+    // Scan new characters until the end of the file
+    while(fscanf(ifp, "%c", &current)!=EOF)
+    {
+        // Check for a potential comment
+        if(current=='/' && commentsIncluded==0)
+        {
+            // Read another character
+            fscanf(ifp, "%c", &current);
+            // Confirm whether or not a comment has been found
+            if(current=='*')
+            {
+                // Replace initial two comment characters with two spaces
+                printf("  ");
+                // Set halt to 0 until end of comment
+                halt=0;
+                // Find the end of the comment
+                while(!halt)
+                {
+                    fscanf(ifp, "%c", &current);
+                    // Replace comment characters with spaces
+                    printf(" ");
+                    // Check for potential end of comment
+                    if(current=='*')
+                    {
+                        fscanf(ifp, "%c", &current);
+                        // Replace comment characters with spaces
+                        printf(" ");
+                        // Check for end of comment
+                        if(current=='/')
+                        {
+                            // End of comment found
+                            halt=1;
+                        }
+                    }
+                }
+            }
+            // Not a comment, print the two characters
+            else
+            {
+                printf("/");
+                printf("%c", current);
+            }
+        }
+        // Not a comment, print the character
+        else
+        {
+            printf("%c", current);
+        }
+    }
+    // Line break
+    printf("\n");
 }
 
 
