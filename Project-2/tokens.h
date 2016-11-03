380950e5e0d3eb5f2fbe935bf029bea6c84715a8
barrond <barrond@users.noreply.github.com>
2016-11-03 19:40:54 -0400

    Update tokens.h
    Added again, trouble with github

--- Project-2/tokens.h
+++ Project-2/tokens.h
@@ -1,5 +1,6 @@
 #ifndef TOKENS_H_INCLUDED
 #define TOKENS_H_INCLUDED
+#include <stdio.h>
 
 typedef enum token {nulsym = 1, identsym = 2, numbersym = 3, plussym = 4, minussym = 5, multsym = 6, slashsym = 7, oddsym = 8,
     eqlsym = 9, neqsym = 10, lessym = 11, leqsym = 12,
@@ -7,6 +8,11 @@ typedef enum token {nulsym = 1, identsym = 2, numbersym = 3, plussym = 4, minuss
     becomessym = 20, beginsym = 21, endsym = 22, ifsym = 23, thensym = 24, whilesym = 25, dosym = 26, callsym = 27,
     constsym = 28, varsym = 29, procsym = 30, writesym = 31, readsym = 32, elsesym = 33} token_type;
 
-token_type get_token();
+typedef struct tok_prop{
+    char id[12];
+    int val;
+}tok_prop;
+
+token_type get_token(FILE* ifp, tok_prop *properties);
 
 #endif // TOKENS_H_INCLUDED
