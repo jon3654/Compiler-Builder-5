4219d0dd418e3d495fd1a97e01dae997206bf756
barrond <barrond@users.noreply.github.com>
2016-11-08 17:52:27 -0500

    Update parser.h
    Updated parameters
    
    Includes tokens.h

--- Project-2/parser.h
+++ Project-2/parser.h
@@ -1,22 +1,23 @@
 #ifndef PARSER_H_INCLUDED
 #define PARSER_H_INCLUDED
+#include "tokens.h"
 
 void parser(FILE* ifp);
 void enter();
-void lookup();
+int lookup();
 void error(int num);
 
 // main parser function prototypes
-void program();
-void block();
-void const_decl();
-void var_dec();
-void statement();
-void condition();
-int rel_op(int token);
-void expression();
-void term();
-void factor();
+void program(FILE* ifp, tok_prop *properties);
+void block(FILE* ifp, tok_prop *properties, token_type *token);
+void const_decl(FILE* ifp, tok_prop *properties, token_type *token);
+void var_dec(FILE* ifp, tok_prop *properties, token_type *token);
+void statement(FILE* ifp, tok_prop *properties, token_type *token);
+void condition(FILE* ifp, tok_prop *properties, token_type *token);
+int rel_op(token_type token);
+void expression(FILE* ifp, tok_prop *properties, token_type *token);
+void term(FILE* ifp, tok_prop *properties, token_type *token);
+void factor(FILE* ifp, tok_prop *properties, token_type *token);
 void number();
 void ident();
 void digit();
