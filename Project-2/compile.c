b90ebcf35bb285353e2d625f609fa6eedae81a49
barrond <barrond@users.noreply.github.com>
2016-11-12 19:18:06 -0500

    Update compile.c
    Compile.c now calls other files and initializes counter

--- Project-2/compile.c
+++ Project-2/compile.c
@@ -1,9 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "parser.h"
 #include "tokens.h"
 #include "generator.h"
 
 int main(int argc, char* argv[]){
+    // Open the input/output file
+    FILE* ifp = fopen(argv[1], "r");
+    FILE* ofp = fopen(argv[2], "w");
 
+    // Create a structure that stores the token properties
+    tok_prop properties;
+
+    // Initialize counter to 0
+    cx=0;
+
+    // Parse the input
+    program(ifp, ofp, &properties);
+
+    // Generate intermediate code
+    generate(ofp);
 
     return 0;
 }
