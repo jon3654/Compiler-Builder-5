459a4b87f0a8bd0c0b4661ac70ed4f5722424a7a
barrond <barrond@users.noreply.github.com>
2016-11-12 19:24:51 -0500

    Update generator.h
    Function prototypes and required variables/structs added

--- Project-2/generator.h
+++ Project-2/generator.h
@@ -1,5 +1,26 @@
 #ifndef GENERATOR_H_INCLUDED
 #define GENERATOR_H_INCLUDED
+#include <stdio.h>
+
+#define CODE_SIZE 5000
+
+enum opcode {LIT = 1, OPR, LOD, STO, CAL, INC, JMP, JPC, SIO};
+
+enum ar_log_operation {RET = 0, NEG, ADD, SUB, MUL, DIV, ODD, MOD, EQL, NEQ, LSS, LEQ, GTR, GEQ};
+
+// Function prototypes
+void emit(int op, int level, int modifier);
+void generate(FILE* ofp);
+
+typedef struct code_pm0 {
+    int op;
+    int l;
+    int m;
+} pm0;
+
+pm0 code[CODE_SIZE];
+
+int cx; // Global counter
 
 
 #endif // GENERATOR_H_INCLUDED
