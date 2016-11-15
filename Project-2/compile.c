676321c07c8a5d9beb3b180add4870a5f28fe6b6
Jonathan Brownlee <jonathanbrownlee@knights.ucf.edu>
2016-11-15 16:27:05 -0500

    Added init_array() call to compile.c. Edited MakeFile for debugging.

--- Project-2/compile.c
+++ Project-2/compile.c
@@ -9,17 +9,18 @@ int main(int argc, char* argv[]){
     FILE* ifp = fopen(argv[1], "r");
     FILE* ofp = fopen(argv[2], "w");
 
+    if(ifp == NULL) printf("Error: File not found./n");
     // Create a structure that stores the token properties
     tok_prop properties;
 
     // Initialize counter to 0
     cx=0;
 
+    init_array();
     // Parse the input
     program(ifp, &properties);
 
     // Generate intermediate code
     generate(ofp);
-
     return 0;
 }
