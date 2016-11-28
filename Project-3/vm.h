3c2d1f0da45eebf1decae2ddd7b0f8a9dc4edafb
barrond <barrond@users.noreply.github.com>
2016-11-28 16:00:22 -0500

    Update vm.h
    Added function prototypes to vm.h

--- Project-3/vm.h
+++ Project-3/vm.h
@@ -1,6 +1,10 @@
 #ifndef VM_H_INCLUDED
 #define VM_H_INCLUDED
 
+// Function prototypes
 int vm(char *file);
+void execute(int opcode, int level, int modifier, char *str, int *lcheck, int *mcheck, int *outcheck, int *out, int *inpcheck,
+int *in, int *hlt, int *pc, int *bp, int *sp);
+int base(int level, int b);
 
 #endif
